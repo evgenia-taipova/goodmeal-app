b9741dcc5c1a98cf953c6067c24a90cb57b8a513
kbadrat <vlad.ren@icloud.com>
2024-12-21 17:15:10 +0100

    fix: resolve issues with login page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import CardsSection from "./components/CardsSection";
-// import Footer from "./components/Footer";
-// import Header from "./components/Header";
-// import LogIn from "./components/LogIn/LogIn";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import LogIn from "./components/LogIn/LogIn";
 
 function App() {
     return (
         <>
             {/* <Footer /> */}
             {/* <Header /> */}
-            {/* <LogIn /> */}
-            <CardsSection />
+            <LogIn />
+            {/* <CardsSection /> */}
         </>
     );
 }
